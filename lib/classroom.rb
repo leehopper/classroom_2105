b32dc34946f1e166549ac3cc01901fb62bb19cbf
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-12 14:59:18 -0600

    Build method add_student and unskip test

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,10 +1,14 @@
 class Classroom
-  attr_reader :subject, :capacity, :students 
+  attr_reader :subject, :capacity, :students
 
   def initialize (subject, capacity)
     @subject = subject
     @capacity = capacity
     @students = []
+  end
+
+  def add_student(student)
+    @students << student
   end
 
 end
