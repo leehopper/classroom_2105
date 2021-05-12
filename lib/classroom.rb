4efeb046c7e5dab2858bf549cca45e0ec7e22b98
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-12 15:45:30 -0600

    Build over_capacity method

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,5 +1,5 @@
 class Classroom
-  attr_reader :subject, :capacity, :students, :upcase_students
+  attr_reader :subject, :capacity, :students
 
   def initialize (subject, capacity)
     @subject = subject
@@ -12,11 +12,23 @@ class Classroom
   end
 
   def yell_at_students
-    @upcase_students = []
+    upcase_students = []
     @students.each do |student|
       upcase_students << student.upcase
     end
-    @upcase_students
+    upcase_students
   end
 
+  def over_capacity
+    student_counter = 0
+    @students.each do |student|
+      student_counter += 1
+    end
+    if @capacity >= student_counter
+      capacity_test = false
+    elsif @capacity < student_counter
+      capacity_test = true
+    end
+    capacity_test
+  end
 end
